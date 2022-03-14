ddb460d4d526902dac891559511c96f4d1feac55
胡玖 <hujiu@qq.com>
2022-03-14 22:02:51 +0800

    111

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login">
-    <img class="bgImg" src="../../assets/home_top_bg.jpg" alt="">
+    <img class="bgImg" src="../../assets/home_top_bg.jpg" alt="" />
     <div class="login-container">
       <div class="title">
         <h1>博客用户登录</h1>
@@ -17,9 +17,19 @@
       <div class="otherLogin">
         <div class="otherTitle">第三方登录</div>
         <div class="otherBtn">
-          <button><a href="https://graph.qq.com/oauth2.0/show?which=Login&display=pc&response_type=code&client_id=101993751&redirect_uri=http://www.decunt.com/home"><img src="../../assets/images/QQ.png" alt=""></a></button>
-          <button><img src="../../assets/images/WeChat.png" alt=""></button>
-          <button><a href="https://github.com/login/oauth/authorize?client_id=9bc20c1d36f7d024515b"><img src="../../assets/images/GitHub.png" alt=""></a></button>
+          <button>
+            <a
+              href="https://graph.qq.com/oauth2.0/show?which=Login&display=pc&response_type=code&client_id=101993751&redirect_uri=http://www.decunt.com/home"
+              ><img src="../../assets/images/QQ.png" alt=""
+            /></a>
+          </button>
+          <button><img src="../../assets/images/WeChat.png" alt="" /></button>
+          <button>
+            <a
+              href="https://github.com/login/oauth/authorize?client_id=9bc20c1d36f7d024515b"
+              ><img src="../../assets/images/GitHub.png" alt=""
+            /></a>
+          </button>
         </div>
       </div>
       <div class="goRegister">
@@ -30,17 +40,16 @@
 </template>
 
 <script setup>
-import { BoldOutlined } from '@ant-design/icons-vue';
-import { useRouter } from 'vue-router';
+import { BoldOutlined } from "@ant-design/icons-vue";
+import { useRouter } from "vue-router";
 import LoginForm from "../../components/LoginForm";
-import { ref } from 'vue'
+import { ref } from "vue";
 
-  const activeKey = ref('1')
-
-  const router = useRouter();
-  const goRegister = () => {
-    router.push('register')
-  }
+const activeKey = ref("1");
+const router = useRouter();
+const goRegister = () => {
+  router.push("register");
+};
 </script>
 
 <style lang="scss" scoped>
@@ -69,13 +78,13 @@ import { ref } from 'vue'
     width: 420px;
     height: 620px;
     padding: 16px 32px;
-    opacity: .9;
+    opacity: 0.9;
     background-color: #fff;
     .otherLogin {
       display: flex;
       justify-content: space-between;
       align-items: center;
-    button {
+      button {
         line-height: 1;
         align-items: center;
         width: 40px;
@@ -84,15 +93,14 @@ import { ref } from 'vue'
         margin-left: 16px;
         padding: 9px;
         border-radius: 50%;
-        border-color: rgba(0,0,0,.12);
+        border-color: rgba(0, 0, 0, 0.12);
         background-color: #fff;
         cursor: pointer;
         &:hover {
-          background-color: rgba(0,0,0,.12);
+          background-color: rgba(0, 0, 0, 0.12);
         }
         img {
           width: 20px;
-
         }
       }
     }
