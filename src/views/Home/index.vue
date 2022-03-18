8caad4462f84333f4551010eb3f1281b50dfc642
_xushuai <xushuai726@foxmail.com>
2022-03-18 10:32:20 +0800

    test: jenkins

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -13,8 +13,8 @@ import {loginByGithub, loginByQQ} from '../../api/home'
 import { message } from 'ant-design-vue'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
-import watermark from 'watermark-dom'
 import router from "../../router";
+import watermark from '@/utils/watermark'
 const store = useStore()
 const route = useRoute()
 const init = async () => {
@@ -22,13 +22,13 @@ const init = async () => {
     switch (localStorage.getItem('loginType')) {
       case 'qq':
         // qq
-        const {code,data} = await loginByQQ(route.query.code).catch(err=>{
-          return message.error('登录失败')
-        })
+        const {code,data,message:msg} = await loginByQQ(route.query.code)
         if (code === 200) {
           store.commit('app/setToken', data.token)
           store.commit('app/setUserInfo', data.user)
           router.go(0)
+        } else {
+          message.error(msg)
         }
       break;
       case 'wx':
@@ -36,9 +36,7 @@ const init = async () => {
       break;
       case 'github':
         // github
-        const res = await loginByGithub(route.query.code).catch((err) => {
-          return message.error('登录失败')
-        })
+        const res = await loginByGithub(route.query.code)
         if (res.code === 200) {
           store.commit('app/setToken', res.data.token)
           store.commit('app/setUserInfo', res.data.user)
@@ -47,29 +45,9 @@ const init = async () => {
       break;
     }
   }
-  watermark.init({
-    watermark_id: 'wm_div_id',          //水印总体的id
-    watermark_prefix: 'mask_div_id',    //小水印的id前缀
-    watermark_txt:"BLOG",               //水印的内容
-    watermark_x:20,                     //水印起始位置x轴坐标
-    watermark_y:20,                     //水印起始位置Y轴坐标
-    watermark_rows:0,                   //水印行数
-    watermark_cols:0,                   //水印列数
-    watermark_x_space:100,              //水印x轴间隔
-    watermark_y_space:50,               //水印y轴间隔
-    watermark_font:'微软雅黑',            //水印字体
-    watermark_color:'black',            //水印字体颜色
-    watermark_fontsize:'18px',          //水印字体大小
-    watermark_alpha:0.15,               //水印透明度，要求设置在大于等于0.005
-    watermark_width:100,                //水印宽度
-    watermark_height:100,               //水印长度
-    watermark_angle:15,                 //水印倾斜度数
-    watermark_parent_width:0,           //水印的总体宽度（默认值：body的scrollWidth和clientWidth的较大值）
-    watermark_parent_height:0,          //水印的总体高度（默认值：body的scrollHeight和clientHeight的较大值）
-    watermark_parent_node:null          //水印插件挂载的父元素element,不输入则默认挂在body上
-  })
 }
 init()
+watermark
 </script>
 
 <style lang="scss" scoped>
@@ -77,8 +55,8 @@ init()
   width: 100%;
   height: 100%;
   overflow-y: auto;
-  background-color: #ccc;
-  //background: url('../../../src/assets/home_bg1.png');
+  //background-color: #ccc;
+  background: url('../../../src/assets/homeBg.jpg');
   background-size: 100% 100%;
   background-clip: content-box;
   .content {
